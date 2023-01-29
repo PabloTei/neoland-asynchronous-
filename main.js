885795713dbce2b3ba83ab79a9d8b42bd2021582
PabloTei <pteijeirolopez@gmail.com>
2023-01-29 22:04:05 +0100

    iteration20

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ myButton.addEventListener("click", (ev) => {
 
 // 1.3 En base al ejercicio anterior. Crea dinamicamente un elemento  por cada petición a la api que diga...'El nombre X tiene un Y porciento de ser de Z' etc etc.EJ: El nombre Pepe tiene un 22 porciento de ser de ET y un 6 porciento de ser de MZ.
 
-const baseUrl = 'https://api.nationalize.io'
+/*const baseUrl = 'https://api.nationalize.io'
 
 const myButton = document.querySelector("button");
 const myInput = document.querySelector("input");
@@ -46,8 +46,38 @@ const myFunction = async (name) => {
 myButton.addEventListener("click", (ev) => {
     myFunction(myInput.value);
     
-})
+})*/
 
 
 
 // 1.4 En base al ejercicio anterior, crea un botón con el texto 'X' para cada uno de los p que hayas insertado y que si el usuario hace click en este botón eliminemos el parrafo asociado.
+
+
+
+
+
+
+
+// 2.1 Gestiona la siguiente promesa para esperar a ejecutar el console usando async-await.
+
+const runTimeOut = () => {
+    const promise = new Promise((resolve) => {
+        setTimeout(function () {
+            resolve();
+        }, 2000);
+    })
+
+    promise.then(() => {console.log('Time out!')})
+};
+
+runTimeOut();
+
+
+
+// 2.2 Convierte la siguiente función con un fetch utilizando async-await. Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
+
+function getCharacters () {
+    fetch('https://rickandmortyapi.com/api/character').then(res => res.json()).then(characters => console.log(characters));
+}
+
+getCharacters();
