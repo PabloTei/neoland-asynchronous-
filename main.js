3ba01c5ac5fc033c12bb1b182a34d7e6373c961d
PabloTei <pteijeirolopez@gmail.com>
2023-01-30 11:33:24 +0100

    iteration1

--- main.js
+++ main.js
@@ -61,17 +61,15 @@ myButton.addEventListener("click", (ev) => {
 // 2.1 Gestiona la siguiente promesa para esperar a ejecutar el console usando async-await.
 
 const runTimeOut = async () => {
-    const promise =  new Promise((resolve) => {
+    await new Promise((resolve) => {
         setTimeout(function () {
             resolve();
         }, 2000);
     })
-
-    await promise.then(() => {console.log('Time out!')})
+    console.log("Hola");
 };
 
 runTimeOut();
-
 
 
 // 2.2 Convierte la siguiente función con un fetch utilizando async-await. Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
